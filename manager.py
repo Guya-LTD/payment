f5da3fa18565efd2be97f2efbc90327744736e80
Simonbelete <simonbelete@gmail.com>
2020-06-28 17:18:42 +0300

    run doc locally setup

---
+++ manager.py
@@ -0,0 +1,20 @@
+# @Copyright (C) Guya , PLC - All Rights Reserved (As Of Pending...)
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.txt', which is part of this source code package.
+
+# @author [Simon Belete](https://github.com/Simonbelete)
+# @project Guya E-commerce & Guya Express
+# @Date:   2020-05-13T03:14:09+03:00
+# @Email:  simonbelete@gmail.com
+# @Project: Guya E-commerce's & Guya Express's : Guy's User Service
+# @Last modified by:   admin
+# @Last modified time: 2020-05-13T21:46:08+03:00
+
+""
+@Copyright (C) Guya , PLC - All Rights Reserved (As Of Pending...)
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+""
