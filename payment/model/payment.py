63ce023f1093d7038506d49268d43997f35110b9
Simonbelete <simonbelete@gmail.com>
2020-09-25 10:58:09 +0000

    feat(models)payment and transaction models

---
+++ payment/model/payment.py
@@ -0,0 +1,32 @@
+"""Header
+
+Copyright
+---------
+    Copyright (C) Guya , PLC - All Rights Reserved (As Of Pending...)
+    Unauthorized copying of this file, via any medium is strictly prohibited
+    Proprietary and confidential
+
+LICENSE
+-------
+    This file is subject to the terms and conditions defined in
+    file 'LICENSE.txt', which is part of this source code package.
+
+Authors
+-------
+    * [Simon Belete](https://github.com/Simonbelete)
+ 
+Project
+-------
+    * Name: 
+        - Guya E-commerce & Guya Express
+    * Sub Project Name:
+        - Payment Service
+    * Description
+        - Payment api
+"""
+
+"""Base Transaction Entity"""
+
+class Payment:
+    """A Base Model Representation of Transaction Entity"""
+    pass
